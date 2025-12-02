3f22ddbec4d722c43405d1395a50e37131bec010
Minhyeok Choi <chlals12267@gmail.com>
2025-12-02 23:42:39 +0900

    feat(favorite) : add Favorite Entity

---
+++ src/main/java/com/multi/travel/domain/favorite/entity/Favorite.java
@@ -0,0 +1,48 @@
+package com.multi.travel.domain.favorite.entity;
+
+/*
+ * Please explain the class!!!
+ *
+ * @filename    : Favorite
+ * @author      : Choi MinHyeok
+ * @since       : 25. 12. 2. 화요일
+ */
+
+import com.multi.travel.domain.place.entity.Place;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(
+        name = "tb_fav",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"member_id", "place_id"})
+)
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "member_id")
+    //private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @Column(name = "is_favorite", length = 1)
+    private String isFavorite;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
