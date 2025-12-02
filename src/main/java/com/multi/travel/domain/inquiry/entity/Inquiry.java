78d4783c2afe7989d64e99f5ef55b2fc95e32184
Minhyeok Choi <chlals12267@gmail.com>
2025-12-02 23:43:46 +0900

    feat(inquiry) : add Inquiry Entity and Enum(StatusType, InquiryType)

---
+++ src/main/java/com/multi/travel/domain/inquiry/entity/Inquiry.java
@@ -0,0 +1,74 @@
+package com.multi.travel.domain.inquiry.entity;
+
+/*
+ * Please explain the class!!!
+ *
+ * @filename    : Inquiry
+ * @author      : Choi MinHyeok
+ * @since       : 25. 12. 2. 화요일
+ */
+
+import com.multi.travel.domain.inquiry.enums.InquiryType;
+import com.multi.travel.domain.inquiry.enums.StatusType;
+import com.multi.travel.domain.place.entity.Place;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "tb_inq")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Inquiry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "member_id")
+    //private Member questioner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @Column(length = 200, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "answer_content", columnDefinition = "TEXT")
+    private String answerContent;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "answer_id")
+    //private Member author;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InquiryType inquiryType;
+
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusType status = StatusType.PENDING;
+
+
+    @CreatedDate
+    @Column(name = "create_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "update_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/multi/travel/domain/inquiry/enums/StatusType.java
@@ -0,0 +1,16 @@
+package com.multi.travel.domain.inquiry.enums;
+
+/*
+ * Please explain the class!!!
+ *
+ * @filename    : InquiryType
+ * @author      : Choi MinHyeok
+ * @since       : 25. 12. 2. 화요일
+ */
+
+
+public enum StatusType {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}
