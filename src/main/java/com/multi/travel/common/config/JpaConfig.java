852825e453c165e39fc49374b1275066d4ddd5a9
Minhyeok Choi <chlals12267@gmail.com>
2025-12-02 23:45:49 +0900

    feat(config) : add JpaConfig

---
+++ src/main/java/com/multi/travel/common/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.multi.travel.common.config;
+
+/*
+ * Please explain the class!!!
+ *
+ * @filename    : JpaConfig
+ * @author      : Choi MinHyeok
+ * @since       : 25. 12. 2. 화요일
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
